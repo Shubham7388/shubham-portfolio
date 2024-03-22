bc629eb3d522e516e83a4ec6fcc6f28189937847
Shubham Chaurasia <chaurasiashubham41@gmail.com>
2024-03-23 00:54:13 +0530

    Add some functionality

---
+++ src/components/Contact/contact.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './contact.css'
+
+const Contact = () => {
+  return (
+        <section className="contactpage">
+            <div id="clients">
+                <h1 className="contactTitle">My Clients</h1>
+                <p className="clientDesc">I had the oppartunity to work with a </p>
+            </div>
+            <div id="contact">
+
+            </div>
+        </section>
+    )
+}
+
+export default Contact;
--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './works.css';
+import portfolio1 from '../../assets/portfolio-1.png'
+import portfolio2 from '../../assets/portfolio-2.png'
+import portfolio3 from '../../assets/portfolio-3.png'
+import portfolio4 from '../../assets/portfolio-4.png'
+import portfolio5 from '../../assets/portfolio-5.png'
+import portfolio6 from '../../assets/portfolio-6.png'
+
 
 const Works = () => {
   return (
     <section id='works'>
-      works done
+      <h2 className='worksTitle'>My Portfolio</h2>
+      <span className="workdesc">A software developer designs, builds, programs, deploys, and maintains software for computers, mobile devices, and websites. They work with teams of coders to create software programs and systems that meet the needs of users. Some of their responsibilities include.</span>
+      <div className="worksImgs">
+        <img src={portfolio1} alt="" className="worksImg" />
+        <img src={portfolio2} alt="" className="worksImg" />
+        <img src={portfolio3} alt="" className="worksImg" />
+        <img src={portfolio4} alt="" className="worksImg" />
+        <img src={portfolio5} alt="" className="worksImg" />
+        <img src={portfolio6} alt="" className="worksImg" />
+      </div>
+      <button className="workBtn">See More</button>
     </section>
   )
 }

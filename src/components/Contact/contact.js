47a2fe39135b0726e6282a5d6bccdcad020bdf5c
Shubham Chaurasia <chaurasiashubham41@gmail.com>
2024-04-08 22:14:32 +0530

    changes done

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.css'
 import walmartImg from '../../assets/walmart.png'
 import microsoftImg from '../../assets/microsoft.png'
@@ -8,8 +8,29 @@ import instaIcon from '../../assets/instagram.png'
 import facebookIcon from '../../assets/facebook-icon.png'
 import twitterIcon from '../../assets/twitter.png'
 import youTubeIcon from '../../assets/youtube.png'
+import emailjs from '@emailjs/browser';
+
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs
+          .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
+            publicKey: 'YOUR_PUBLIC_KEY',
+          })
+          .then(
+            () => {
+              console.log('SUCCESS!');
+            },
+            (error) => {
+              console.log('FAILED...', error.text);
+            },
+          );
+      };
+
   return (
         <section id="contactpage">
             <div id="clients">
@@ -30,7 +51,7 @@ const Contact = () => {
                 <span className="contactDesc">
                     Please fill out the form below to discuss any work opportunities.
                 </span>
-                <form className="contactForm">
+                <form className="contactForm" ref={form} onSubmit={sendEmail}>
                     <input type="text" className="name" placeholder='Your Name' />
                     <input type="email" className="email" placeholder='Your Email' />
                     <textarea className="msg" name="message" rows="5" placeholder='Your Message'></textarea>
